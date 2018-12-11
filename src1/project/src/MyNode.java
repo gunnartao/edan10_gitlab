9034a22a2bf262c41da41c0fd0b2fa2ea9b311bc
Hugo Jernberg <Hugo@Hugos-MacBook.local>
2018-12-11 11:38:05 +0100

    Add attribute.

--- src1/project/src/MyNode.java
+++ src1/project/src/MyNode.java
@@ -16,6 +16,7 @@ public class MyNode {
   private MyNode left;
   private MyNode right;
   private int value;
+  private boolean notImportant;
 
   /**
    * Class constructor initializing this node's value.
